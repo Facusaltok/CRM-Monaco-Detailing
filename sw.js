0f0f3f7d6d31cdf041f7192ebdb2e4753d77c434
Facusaltok <facundodamiansalto@gmail.com>
2025-08-18 13:08:54 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,16 @@
 const CACHE = "monaco-crm-v1";
-const ASSETS = ["./index.html","./manifest.webmanifest"];
+const ASSETS = ["./index.html","./dashboard.html","./supabaseClient.js","./manifest.webmanifest"];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
 });
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
