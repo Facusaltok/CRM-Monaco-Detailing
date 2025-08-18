661533ebb2c79213d0b02a92c8a73cf57d7570e6
Facusaltok <facundodamiansalto@gmail.com>
2025-08-18 14:16:59 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,6 @@
-const CACHE = "monaco-crm-v2";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./dashboard.html",
-  "./supabaseClient.js",
-  "./manifest.webmanifest"
-];
+const CACHE = "monaco-crm-v3";
+const ASSETS = ["./","./index.html","./dashboard.html","./supabaseClient.js","./manifest.webmanifest"];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
-  );
-});
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); });
+self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
