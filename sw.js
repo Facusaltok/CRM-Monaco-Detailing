d058ab7f1941fa28d755868ab765bcab004b2055
Facusaltok <facundodamiansalto@gmail.com>
2025-08-19 19:47:56 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// sw.js
-self.addEventListener("install", (e) => self.skipWaiting());
-self.addEventListener("activate", (e) => self.clients.claim());
-// sin handler de 'fetch' para evitar warning "no-op fetch handler"
