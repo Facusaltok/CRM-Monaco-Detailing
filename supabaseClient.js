e2aa2ee386f9b3703980a1199eb2d19e7b727d77
Facusaltok <facundodamiansalto@gmail.com>
2025-08-20 07:34:17 -0300

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,9 +0,0 @@
-<!-- PON ESTO EN index.html y dashboard.html, dentro de <head> -->
-<script src="https://unpkg.com/@supabase/supabase-js@2"></script>
-<script>
-  // Cliente Supabase global (sin módulos, compatible iOS)
-  window.supabaseClient = window.supabase.createClient(
-    "https://nqokfvddbpjqrsfflntk.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xb2tmdmRkYnBqcXJzZmZsbnRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2NDE5OTgsImV4cCI6MjA3MTIxNzk5OH0.A5CzSNjHJ9y012vJjDunzYMTzoxGwu6RV88OmKYOUGo"
-  );
-</script>
